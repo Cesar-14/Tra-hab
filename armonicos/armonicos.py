88a3f592dd1b54398a981fbf7fc01b0bc39a2d91
Cesar Escobedo <cesarescobedo@MacBook-Pro-de-Cesar-2.local>
2022-02-28 19:35:32 -0700

    armonicas_clase

--- armonicos/armonicos.py
+++ armonicos/armonicos.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.io.wavfile import write
 
 frecuencia_muestreo = 44100
-frecuencia = 200
+frecuencia = 450
 tiempos = np.linspace(0.0,1.0,frecuencia_muestreo)
 amplitud = np.iinfo(np.int16).max
 
@@ -34,4 +34,30 @@ frecuencias = np.fft.rfftfreq(cantidad_muestras, periodo_muestreo)
 
 plt.figure()
 plt.plot(frecuencias, np.abs(transformada))
+# plt.show()
+
+# Ejercicio Clase
+
+# 1.- Obtener en Hz las frecuencias de los armonicos de la señal
+
+print(frecuencias[transformada > 100000])
+    
+# 2.- Aplicarle un filtro pasabajas que solo deje pasar la Freq Fundamental y luego aplicarle una transformada inversa
+
+pasa_bajas = transformada.copy()
+pasa_bajas[frecuencias > frecuencia] *= 0
+
+pasa_bajas_data = np.fft.irfft(pasa_bajas)
+
+
+# graficarlas y crear un archivo wav para escucharlas
+
+write("triangulo_pasabajas.wav", frecuencia_muestreo, pasa_bajas_data.astype(np.int16))
+
+plt.plot(frecuencias, np.abs(pasa_bajas), label = "Filtro pasabajas")
+plt.legend()
+plt.show()
+
+plt.plot(tiempos,pasa_bajas_data, label="Audio pasabajas")
+plt.legend()
 plt.show()
